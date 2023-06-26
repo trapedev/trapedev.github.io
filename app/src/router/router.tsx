c5d9940cc309623635a20b2915e6a17d547a3551
trapedev <mvemtae5757@outlook.jp>
2023-06-26 11:50:07 +0900

    commit all changes

--- app/src/router/router.tsx
+++ app/src/router/router.tsx
@@ -14,30 +14,10 @@ const Router = (): JSX.Element => {
       <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-        </Routes>
-      </HashRouter>
-      <HashRouter>
-        <Routes>
           <Route path="/education" element={<Education />} />
-        </Routes>
-      </HashRouter>
-      <HashRouter>
-        <Routes>
           <Route path="/experiences" element={<Experiences />} />
-        </Routes>
-      </HashRouter>
-      <HashRouter>
-        <Routes>
           <Route path="/academic-paper" element={<AcademicPaper />} />
-        </Routes>
-      </HashRouter>
-      <HashRouter>
-        <Routes>
           <Route path="/accomplishments" element={<Accomplishments />} />
-        </Routes>
-      </HashRouter>
-      <HashRouter>
-        <Routes>
           <Route path="/skills" element={<Skills />} />
         </Routes>
       </HashRouter>
