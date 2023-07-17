37e03b7d9e0856eb98a459ef2ee6e32fb1baca67
trapedev <mvemtae5757@outlook.jp>
2023-07-18 01:37:46 +0900

    commit all changes

--- app/src/pages/home.tsx
+++ app/src/pages/home.tsx
@@ -7,11 +7,18 @@ const Home = () => {
     <Template name={"Home"}>
       <BaseContainer>
         <BaseItem xs={12}>
-          <Typography gutterBottom variant="h2" component="div">
-            Hi There !
-          </Typography>
           <Typography gutterBottom variant="h4" component="div">
-            I'm Keiichiro KIMURA
+            Biography
+          </Typography>
+          <Typography gutterBottom variant="h6" component="div">
+            My name is Keiichiro KIMURA, and I am a first-year master's student
+            in the Graduate School of Engineering at Kobe University. I am
+            conducting research in information security, with my research theme
+            being the study of vulnerabilities in wireless communication, such
+            as HTTPS and Bluetooth. In addition to my studies, I am involved in
+            the development of a web application called 'JUKSL', primarily
+            handling the implementation of the backend. I also have an enduring
+            love for Gopher, the character of Golang.
           </Typography>
         </BaseItem>
       </BaseContainer>
