a18f02cab40df655f3d061cb2d2a7adb07b8184b
trapedev <mvemtae5757@outlook.jp>
2023-09-13 22:08:48 +0900

    commit all changes

--- app/src/pages/academicPaper.tsx
+++ app/src/pages/academicPaper.tsx
@@ -13,8 +13,9 @@ const AcademicPaper = () => {
     <Template name={"Academic Paper"}>
       <BaseContainer>
         <BaseItem xs={12}>
-          <First />
-          <Second />
+          <CSS2023 />
+          <CSS2022 />
+          <FIT2022 />
         </BaseItem>
       </BaseContainer>
     </Template>
@@ -23,12 +24,12 @@ const AcademicPaper = () => {
 
 export default AcademicPaper;
 
-const First = () => {
+const CSS2022 = () => {
   return (
     <SingleCardItem>
       <Title
         title={
-          "Implementation of Man-in-the-Middle Attacks that bypass SSL/TLS and allow eavesdropping/falsification ~ Public Wireless LAN Vulnerabilities and Threats ~"
+          "Implementation of Man-in-the-Middle Attacks that bypass SSL/TLS and allow eavesdropping/falsification: Public Wireless LAN Vulnerabilities and Threats"
         }
         hyperLink="https://www.iwsec.org/css/2022/program.html#2A4-I"
       />
@@ -41,12 +42,12 @@ const First = () => {
   );
 };
 
-const Second = () => {
+const FIT2022 = () => {
   return (
     <SingleCardItem>
       <Title
         title={
-          "New Proposals and Threats of Man-in-the-Middle Attacks to Evade SSL/TLS ~ Evaluation and Consideration by Implementation ~"
+          "New Proposals and Threats of Man-in-the-Middle Attacks to Evade SSL/TLS: Evaluation and Consideration by Implementation"
         }
         hyperLink="https://onsite.gakkai-web.net/fit2022/abstract/data/html/program/l.html#s1k"
       />
@@ -55,6 +56,24 @@ const Second = () => {
         title={"Keiichiro Kimura, Yoshiaki Shiraishi, Masakatu Morii"}
       />
       <SecondaryText data="pp.33-40, 2022-08-30" />
+    </SingleCardItem>
+  );
+};
+
+const CSS2023 = () => {
+  return (
+    <SingleCardItem>
+      <Title
+        title={
+          "A New Attack Method on Bluetooth BR/EDR and Its Demonstration: Vulnerability of Power-Saving Mode and Its Threat"
+        }
+        hyperLink="https://www.iwsec.org/css/2023/program.html#3E3"
+      />
+      <Title title={"Computer Security Symposium 2023"} />
+      <SubTitle
+        title={"Keiichiro Kimura, Hiroki Kuzuno, Yoshiaki Shiraishi, Masakatu Morii"}
+      />
+      {/* <SecondaryText data="pp.33-40, 2022-08-30" /> */}
     </SingleCardItem>
   );
 };
