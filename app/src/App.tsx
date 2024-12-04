08cdf5cea76b894d665aab51864b044aa38e48f5
trapedev <mvemtae5757@outlook.jp>
2024-12-05 00:37:32 +0900

    commit all changes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,7 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import { HOME_TITLE } from "./models/constants";
 
 interface Props {
   /**
@@ -24,13 +12,6 @@ interface Props {
 }
 
 const drawerWidth = 240;
-const navItems = [
-  "Home",
-  "Education",
-  "Experience",
-  "Publication",
-  "Achievement",
-];
 
 export default function App(props: Props) {
   const { window } = props;
@@ -40,72 +21,12 @@ export default function App(props: Props) {
     setMobileOpen((prevState) => !prevState);
   };
 
-  const scrollToSection = (sectionId: string) => {
-    const section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
-    }
-    setMobileOpen(false);
-  };
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        {HOME_TITLE}
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton
-              sx={{ textAlign: "center" }}
-              onClick={() => scrollToSection(item.toLowerCase())}
-            >
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
   const container =
     window !== undefined ? () => window().document.body : undefined;
 
   return (
     <Box sx={{ display: "flex", backgroundColor: "#272d33", color: "white" }}>
       <CssBaseline />
-      <AppBar component="nav" sx={{ backgroundColor: "#1A1A1A" }}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-          >
-            {HOME_TITLE}
-          </Typography>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            {navItems.map((item) => (
-              <Button
-                key={item}
-                sx={{ color: "#fff" }}
-                onClick={() => scrollToSection(item.toLowerCase())}
-              >
-                {item}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </AppBar>
       <nav>
         <Drawer
           container={container}
@@ -123,7 +44,7 @@ export default function App(props: Props) {
             },
           }}
         >
-          {drawer}
+          {"This site is temporary unavailable. (closed)"}
         </Drawer>
       </nav>
     </Box>
