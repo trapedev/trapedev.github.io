a4e5f8a20428871fc8ec14c152aa6250b5b1cfab
trapedev <mvemtae5757@outlook.jp>
2025-03-08 01:50:57 +0900

    commit all changes

--- app/src/pages/achv/WH1000XM5AuthVulnFix.tsx
+++ app/src/pages/achv/WH1000XM5AuthVulnFix.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import { SecondaryText, SingleCardItem, SubTitle, Title } from "components/views/ui";
 
 const WH1000XM5AuthVulnFix = () => {
@@ -11,6 +12,27 @@ const WH1000XM5AuthVulnFix = () => {
       <SecondaryText text={"2024年8月，WH-1000XM5に未知の脆弱性（CWE-287 related）が存在することを発見．"} />
       <SecondaryText text={"2024年11月，ベンダーは脆弱性を認めた．"} />
       <SecondaryText text={"2025年2月，v2.4.1としてパッチが適用されたファームウェアがリリース．"} />
+      <Box
+        sx={{
+          position: "relative",
+          paddingBottom: "25.25%", // 16:9 aspect ratio
+          height: 0,
+          overflow: "hidden",
+          maxWidth: "100%",
+          mt: 3,
+        }}
+      >
+        <img
+          src="wh-1000xm5-acks.png"
+          style={{
+            position: "absolute",
+            width: "75%",
+            maxWidth: "800px"
+          }
+        }
+        alt=""
+        />
+      </Box>
     </SingleCardItem>
   );
 };
