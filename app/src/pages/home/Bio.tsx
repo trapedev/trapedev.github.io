6730d93d1afc06efef576f63bd65697b3b4c28fc
trapedev <mvemtae5757@outlook.jp>
2025-03-09 03:11:00 +0900

    commit all changes

--- app/src/pages/home/Bio.tsx
+++ app/src/pages/home/Bio.tsx
@@ -5,12 +5,10 @@ const Bio = () => {
     <>
       <Typography variant="h4">Finding Flaws. Fixing Futures.</Typography>
       <Typography variant="h6">
-        Hi, there! My name is Keiichiro KIMURA, a second-year master's student
-        in the Graduate School of Engineering, Kobe University. My research
-        focuses on vulnerabilities in wireless technologies, such as HTTPS and
-        Bluetooth.
+        Hi, there! I'm Keiichiro KIMURA. My research focuses on vulnerabilities
+        in wireless technologies, such as HTTPS and Bluetooth.
       </Typography>
-      <Typography variant="h6">
+      <Typography variant="h6" sx={{ mt: 5 }}>
         This website will be archived on April 1st, 2025.
       </Typography>
     </>
