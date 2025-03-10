e09873af93875d50e98d5f2641c833cb6bd8ec11
trapedev <mvemtae5757@outlook.jp>
2025-03-10 17:23:57 +0900

    commit all changes

--- app/src/pages/achv/WH1000XM5AuthVulnFix.tsx
+++ app/src/pages/achv/WH1000XM5AuthVulnFix.tsx
@@ -18,16 +18,19 @@ const WH1000XM5AuthVulnFix = () => {
       <SubTitle title={"03/2025"} />
       <SecondaryText
         text={
-          "2024年8月，WH-1000XM5に未知の脆弱性（CWE-287 related）が存在することを発見．"
+          "2024年8月，WH-1000XM5のBluetooth認証プロセスに新たな脆弱性（CWE-287 related）が存在することを発見．HackerOneを通じてSonyへ報告．"
         }
       />
-      <SecondaryText text={"2024年11月，ベンダーは脆弱性を認めた．"} />
+      <SecondaryText text={"2024年11月，Sonyは脆弱性を認めた．"} />
       <SecondaryText
         text={
           "2025年2月，v2.4.1としてパッチが適用されたファームウェアがリリース．"
         }
       />
       <SecondaryText text={"2025年3月，英語と日本語で謝辞が公開された．"} />
+      <SecondaryText text={"本脆弱性は，我々が開発した攻撃ツール「Breaktooth」を用いて発見されたものである．"} />
+      <SecondaryText text={"我々は，ICSS2025@沖縄にて本脆弱性の説明を含む学会発表を行った．"} />
+      <SecondaryText text={"個人的活動であるにも関わらず，脆弱性の再現環境をご提供いただいた葛野弘樹 准教授，白石善明 教授，森井昌克 教授（情報通信研究室ES3）に感謝申し上げます．"} />
       <Box
         sx={{
           position: "relative",
