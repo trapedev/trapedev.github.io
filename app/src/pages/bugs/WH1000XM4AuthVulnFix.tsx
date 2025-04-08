acb14562110ba21747f376862485cbdfd24f7019
trapedev <mvemtae5757@outlook.jp>
2025-04-08 19:42:06 +0900

    Sony, WH-1000XM4's Improper Authentication Vulnerability (Fixed in v2.9.0)

--- app/src/pages/bugs/WH1000XM4AuthVulnFix.tsx
+++ app/src/pages/bugs/WH1000XM4AuthVulnFix.tsx
@@ -1,11 +1,59 @@
-import { SingleCardItem, Title } from "components/views/ui";
+import { Box } from "@mui/material";
+import {
+  SingleCardItem,
+  SubTitle,
+  Title,
+} from "components/views/ui";
 
 const WH1000XM4AuthVulnFix = () => {
   return (
     <SingleCardItem>
       <Title
-        title={"To be appeared in 8th April 2025"}
+        title={
+          "Sony, WH-1000XM4's Improper Authentication Vulnerability (Fixed in v2.9.0)"
+        }
+        hyperLink="https://github.com/trapedev/WH-1000XM4_Auth_Vuln_Fixed_v2.9.0"
       />
+      <SubTitle title={"04/2025"} />
+      <Box
+        sx={{
+          position: "relative",
+          paddingBottom: "20%", // 16:9 aspect ratio
+          height: 0,
+          overflow: "hidden",
+          maxWidth: "100%",
+          mt: 3,
+        }}
+      >
+        <img
+          src="wh-1000xm5-acks-en.png"
+          style={{
+            position: "absolute",
+            width: "75%",
+            maxWidth: "800px",
+          }}
+          alt="wh-1000xm4-acks-en"
+        />
+      </Box>
+      <Box
+        sx={{
+          position: "relative",
+          paddingBottom: "15%", // 16:9 aspect ratio
+          height: 0,
+          overflow: "hidden",
+          maxWidth: "100%",
+        }}
+      >
+        <img
+          src="wh-1000xm5-acks-jp.png"
+          style={{
+            position: "absolute",
+            width: "75%",
+            maxWidth: "800px",
+          }}
+          alt="wh-1000xm4-acks-jp"
+        />
+      </Box>
     </SingleCardItem>
   );
 };
