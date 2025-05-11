d75fa054cccaf30fe8e8e37a3656de5bac6323ed
trapedev <mvemtae5757@outlook.jp>
2025-05-11 16:20:54 +0900

    commit all changes

--- app/src/pages/home/Bio.tsx
+++ app/src/pages/home/Bio.tsx
@@ -5,11 +5,10 @@ const Bio = () => {
     <>
       <Typography variant="h4">Finding Flaws. Fixing Futures.</Typography>
       <Typography variant="h6">
-        Hi, there! I'm Keiichiro KIMURA. My research focuses on vulnerabilities
-        in wireless technologies, such as HTTPS and Bluetooth.
+        Hi, there! I'm Keiichiro KIMURA. Currently navigating my first year in the professional realm as a Product Security Engineer.
       </Typography>
       <Typography variant="h6" sx={{ mt: 2 }}>
-        This website will no longer be updated, except for results/achievements
+        This website is no longer updated, except for results/achievements
         before March 31, 2025.
       </Typography>
     </>
