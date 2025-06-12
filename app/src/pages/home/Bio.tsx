677e2862edd05241804c226191017d8ebaa9a538
trapedev <mvemtae5757@outlook.jp>
2025-06-13 00:45:04 +0900

    commit all changes

--- app/src/pages/home/Bio.tsx
+++ app/src/pages/home/Bio.tsx
@@ -5,8 +5,7 @@ const Bio = () => {
     <>
       <Typography variant="h4">Finding Flaws. Fixing Futures.</Typography>
       <Typography variant="h6">
-        Hi, there! I'm Keiichiro KIMURA.
-        I am currently working as a Product Security Engineer for Sony.
+        Hi, there! I'm Keiichiro KIMURA, currently working as a Product Security Engineer for Sony.
       </Typography>
       {/* <Typography variant="h6" sx={{ mt: 2 }}>
         This website is no longer updated, except for results/achievements
