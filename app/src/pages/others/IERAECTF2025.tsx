c227fcdede7abdd8a5477dcd49d5357b87cd3d0c
trapedev <mvemtae5757@outlook.jp>
2025-06-24 23:39:21 +0900

    commit all changes

---
+++ app/src/pages/others/IERAECTF2025.tsx
@@ -0,0 +1,18 @@
+import { SecondaryText, SingleCardItem, SubTitle, Title } from "components/views/ui";
+
+const IERAECTF2025 = () => {
+    return (
+        <SingleCardItem>
+            <Title
+                title={
+                    "IERAE CTF 2025"
+                }
+                hyperLink="https://gmo-cybersecurity.com/event/ieraectf25/"
+            />
+            <SubTitle title={"06/2025"} />
+            <SecondaryText text={"Ranking: 16 / 538"} />
+        </SingleCardItem>
+    );
+};
+
+export default IERAECTF2025;
